c3a0b680594438c279ebe67cb6c08ccd0752da16
Sourav <souravsaha101@gmail.com>
2021-07-09 01:41:49 +0530

    Header added

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./styles.scss";
+import Logo from "../../assests/45d.jpg";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="wrap">
+        <div className="logo">
+          <img src={Logo} alt="ol" />
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
